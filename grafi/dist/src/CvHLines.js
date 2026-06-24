23a68063619de60c7f8467b6eb2cca7a88bd510f
PerezvictoriaCesar <118016268+perezvictoriacesar@users.noreply.github.com>
2026-06-23 22:42:59 -0600

    Subiendo carpeta del proyecto final

---
+++ grafi/dist/src/CvHLines.js
@@ -0,0 +1,6 @@
+export class CvHLines {
+    paint() {
+        // Método vacío, solo para que compile
+        console.log("CvHLines.paint() llamado (versión dummy)");
+    }
+}
---
+++ grafi/src/CvHLines.ts
@@ -0,0 +1,6 @@
+export class CvHLines {
+  paint(): void {
+    // Método vacío, solo para que compile
+    console.log("CvHLines.paint() llamado (versión dummy)");
+  }
+}
